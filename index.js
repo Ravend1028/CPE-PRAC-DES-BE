188b28f5ac0bf881a4830d60c69a3e5b7d9d9578
Ravend1028 <ravendavid.rd30@gmail.com>
2025-03-05 02:54:37 +0800

    Setup for deployment

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import router from './routes/authRoutes.js';
 import { notFound, errorHandler } from './middlewares/errorHandler.js';
 import connectDB from './configs/db.js';
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 dotenv.config();
 connectDB();
@@ -18,6 +19,19 @@ app.use(cookieParser());
 
 // Auth Routes
 app.use('/api/users', router);
+
+if (process.env.NODE_ENV === 'production') {
+  const __dirname = path.resolve(); // returns current working directory
+  app.use(express.static(path.join(__dirname, 'frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+  });
+} else {
+  app.get('/', (req, res) => {
+    res.send('Server is ready');
+  });
+}
 
 // Error Handlers
 app.use(notFound);
